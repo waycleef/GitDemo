338401fbfc1c89afb790958669fb91239038a519
waycleef <khmalikov@gmail.com>
2022-11-11 18:52:46 +0600

    Test Commit

---
+++ PersonalQuiz_HW8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalQuiz_HW8
+//
+//  Created by Алишер Маликов on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World")
+    }
+
+
+}
+
